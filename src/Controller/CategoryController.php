b9b18dc145e1a166e3c13ae5474f992cb76f8b1a
Алексей Анисимов <ork.04@yandex.ru>
2020-08-16 21:34:42 +0300

    Вывод списка рубрик

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,13 +1,27 @@
-<?php 
+<?php
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Categories;
 use Symfony\Component\HttpFoundation\Response;
 
-class CategoryController
+class CategoryController extends AbstractController
 {
+    /**
+     * @Route("/category", name="category")
+     */
     public function index()
     {
-        return new Response('Test');
+        $categories = $this->getDoctrine()
+            ->getRepository(Categories::class)
+            ->findAll();
+
+
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'Categories',
+            'categories' => $categories,
+        ]);
     }
 }
